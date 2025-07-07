b868869c6b922b82fdc1e1b0391d2420add2ba26
ahmedabd-elkader <ahmedmohamed251201@gmail.com>
2025-07-07 13:40:28 +0300

    CU-8699n7n4c - ✅ Sub-task 2.3.1: Garage Registration Form

--- Parking/src/Routes/RouteList.jsx
+++ Parking/src/Routes/RouteList.jsx
@@ -1,37 +1,37 @@
 
-    import React, { useState, useEffect } from 'react'
-    import {Route ,Routes} from "react-router-dom"
-    import Theme from '../components/Theme'
-    import LoginForm from '../components/LoginForm'
-    import ProtectedRedirect from '../components/ProtectedRedirect';
-    import DriverDashboard from '../components/DriverDashboard';     
-    import GarageDashboard from '../components/GarageDashboard';   
-    import RegisterUser from '../components/RegisterUser'
-    import Home from '../components/Home'
-    import Profile from '../Pages/Profile'
-    import Settings from '../Pages/Settings'
-    import NearbyGarages from "../components/NearbyGarages";
-    import GarageDetails from '../Pages/GarageDetails'
-    import Manual from '../components/Manual';
-    import PasswordResetFlow from "../PasswordResetFlow/pages/PasswordResetFlow";
-    import Activation from '../components/Activation';
+import React, { useState, useEffect } from 'react'
+import { Route, Routes } from "react-router-dom"
+import Theme from '../components/Theme'
+import LoginForm from '../components/LoginForm'
+import ProtectedRedirect from '../components/ProtectedRedirect';
+import DriverDashboard from '../components/DriverDashboard';
+import GarageDashboard from '../components/GarageDashboard';
+import RegisterUser from '../components/RegisterUser'
+import Home from '../components/Home'
+import Profile from '../Pages/Profile'
+import Settings from '../Pages/Settings'
+import NearbyGarages from "../components/NearbyGarages";
+import GarageDetails from '../Pages/GarageDetails'
+import Manual from '../components/Manual';
+import PasswordResetFlow from "../PasswordResetFlow/pages/PasswordResetFlow";
+import Activation from '../components/Activation';
 
-    import OwnerDashboard from '../components/OwnerDashboard'; 
+import OwnerDashboard from '../components/OwnerDashboard';
 
-    import AdminDashboard from '../Pages/AdminDashboard';
-    import AdminProtectedRoute from '../components/AdminWrapper/AdminProtectedRoute';
-    import NotAuthorizedPage from '../components/AdminWrapper/NotAuthorizedPage';
+import AdminDashboard from '../Pages/AdminDashboard';
+import AdminProtectedRoute from '../components/AdminWrapper/AdminProtectedRoute';
+import NotAuthorizedPage from '../components/AdminWrapper/NotAuthorizedPage';
+import GarageRegister from '../components/GarageData/GarageRegister';
 
 
-
-    function RouteList() {
-        const [darkMode, setDarkMode] = useState(() => {
-            return localStorage.getItem("theme") === "dark";
-        });
-        useEffect(() => {
-            document.documentElement.classList.toggle("dark", darkMode);
-            localStorage.setItem("theme", darkMode ? "dark" : "light");
-        }, [darkMode]);
+function RouteList() {
+    const [darkMode, setDarkMode] = useState(() => {
+        return localStorage.getItem("theme") === "dark";
+    });
+    useEffect(() => {
+        document.documentElement.classList.toggle("dark", darkMode);
+        localStorage.setItem("theme", darkMode ? "dark" : "light");
+    }, [darkMode]);
     return (
         <Routes>
             <Route path="/" element={<ProtectedRedirect darkMode={darkMode} setDarkMode={setDarkMode} />} />
@@ -46,10 +46,11 @@
             <Route path="/garages/:id" element={<GarageDetails />} />
             <Route path="/password-reset" element={<PasswordResetFlow darkMode={darkMode} setDarkMode={setDarkMode} />} />
             <Route path="/manual" element={<Manual />} />
-            <Route path='/activation' element={<Activation darkMode={darkMode} setDarkMode={setDarkMode}/>}/>
+            <Route path='/activation' element={<Activation darkMode={darkMode} setDarkMode={setDarkMode} />} />
             <Route path="/dashboard/owner" element={<OwnerDashboard darkMode={darkMode} setDarkMode={setDarkMode} />} /> {/* <--- أضف هذا المسار */}
-            <Route path='/admin'element={<AdminProtectedRoute> <AdminDashboard darkMode={darkMode} setDarkMode={setDarkMode}/> </AdminProtectedRoute>}/>
-            <Route path='/not-authorized' element={<NotAuthorizedPage/>}/>
+            <Route path='/admin' element={<AdminProtectedRoute> <AdminDashboard darkMode={darkMode} setDarkMode={setDarkMode} /> </AdminProtectedRoute>} />
+            <Route path='/not-authorized' element={<NotAuthorizedPage />} />
+            <Route path="/garage/register" element={<GarageRegister darkMode={darkMode} setDarkMode={setDarkMode} />} />
         </Routes>
 
     )
